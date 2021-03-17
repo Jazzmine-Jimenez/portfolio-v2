d6af10ff459b49e1adf89b70a4e57be2460567c4
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2021-03-16 20:46:14 -0700

    fixed width of card container

--- src/home.jsx
+++ src/home.jsx
@@ -7,13 +7,13 @@ export default function Home () {
       {header()}
     </div>
       <div className="container main-container">
-      <div className="row">
+        <div className="row align-middle gx-5">
         <div className="col-5">
           <div className="shadow card-container my-4">
             <div className="card-background">
               <div className="row">
                 <img
-                  className="rounded-circle w-auto p-5"
+                    className="rounded-circle w-75 p-5 mx-auto d-block"
                   src="/images/about-me.jpg"
                   alt="image of me, Jazzmine." />
               </div>
@@ -33,16 +33,16 @@ export default function Home () {
             <div className="row text-center">
               <div className="col-12 ">
                   <a className="btn fs-3" href="mailto:jazzmine.m25@gmail.com" role="button">
-                  <i class="fas fa-envelope"></i>
+                  <i className="fas fa-envelope"></i>
                 </a>
                   <a className="btn fs-3" href="https://www.linkedin.com/in/jazzmine-jimenez/" target="_blank" role="button">
-                  <i class="fab fa-linkedin"></i>
+                  <i className="fab fa-linkedin"></i>
                 </a>
                   <a className="btn fs-3" href="https://github.com/Jazzmine-Jimenez" target="_blank" role="button">
-                  <i class="fab fa-github"></i>
+                  <i className="fab fa-github"></i>
                 </a>
                   <a className="btn fs-3" href="https://drive.google.com/file/d/1xTbeHmWCy8U8smHypAzyIn391CqoDIFZ/view" target="_blank" role="button">
-                  <i class="fas fa-file-download"></i>
+                  <i className="fas fa-file-download"></i>
                 </a>
               </div>
             </div>
@@ -51,23 +51,31 @@ export default function Home () {
         <div className="col-7">
           <h3 className="fs-1 fw-bold py-3">Hello, </h3>
           <h4 className="pb-3">I am a software developer with experience with &nbsp;
-            <i class="fab fa-html5"></i> &nbsp;
-            <i class="fab fa-css3-alt"></i> &nbsp;
-            <i class="fab fa-js-square"></i> &nbsp;
-            <i class="fab fa-react"></i> &nbsp;
-            <i class="fab fa-node"></i> &nbsp;
+            <i className="fab fa-html5"></i> &nbsp;
+            <i className="fab fa-css3-alt"></i> &nbsp;
+            <i className="fab fa-js-square"></i> &nbsp;
+            <i className="fab fa-react"></i> &nbsp;
+            <i className="fab fa-node"></i> &nbsp;
           </h4>
+
+          <a className="btn btn-outline-dark me-3 mb-3 px-4" href="#" role="button">Resume</a>
+          <a className="btn btn-outline-dark mb-3 px-4" href="#" role="button">Projects</a>
+
           <p className="fst-italic">I was introduced to coding about 8 years ago and immediately fell in love.</p>
+
           <p> I have a B.S. in mathematics because I love how difficult math is, the problem solving,
             the logic and how accomplished I feel when I complete a problem.
             I was able to find everything I love about math in coding,
             but the big difference you are able to create something that others can use.
-            After taking that C++ course I considered switching majors but being a first generation
-            college student decided it was more important for me to complete my degree in 4 years.
-            Looking back now I wish I would have taken the extra time, but here I am in the middle
+            </p>
+
+            <p> After taking that C++ course I considered switching majors but being a first generation
+            college student decided it was more important for me to complete my degree in 4 years. </p>
+
+            <p>Looking back now I wish I would have taken the extra time, but here I am in the middle
             of a career change as recent coding bootcamp grad making my
-            dream of coding for a living a reality.
-          </p>
+            dream of coding for a living a reality. </p>
+
         </div>
       </div>
     </div>
