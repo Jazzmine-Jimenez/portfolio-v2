b5a3cfe05506966514417500820b21588cabf9ce
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2021-03-19 12:48:53 -0700

    set up app file and projects page file

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+import Home from './pages/home';
+
+// import NotFound from './pages/not-found';
+
+export default function App(){
+  // const [route, setRoute] = useState(parseRoute(window.location.hash))
+  // console.log(route)
+
+  // renderPage(){
+  //   if (route )
+  // }
+
+    return (
+      <p>hi</p>
+    );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Home from './home'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
 
 ReactDOM.render(
-  <Home />,
+  <App />,
   document.querySelector('#root')
 );
