5fb7bb7b2513201bab610dea9213298bfb06052a
Jazzmine Jimenez <jazzmine.m25@gmail.com>
2021-03-19 13:01:07 -0700

    added in parse route

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import parseRoute from './lib/parse-route';
 import Home from './pages/home';
 
 // import NotFound from './pages/not-found';
 
 export default function App(){
-  // const [route, setRoute] = useState(parseRoute(window.location.hash))
-  // console.log(route)
+  const [route, setRoute] = useState(parseRoute(window.location.hash))
+  console.log(route)
 
   // renderPage(){
   //   if (route )
